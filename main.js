de078faf0c2c676bd0c25f9a573a151fe6b09294
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-01-31 02:18:39 +0500

    media query done project finished fully

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ let profileMenu = document.getElementById('profileMenu');
 let toggleMenu = () => {
     profileMenu.classList.toggle('open-menu');
 }
+
+
+
+
+
+
+let sidebarActivity = document.getElementById('sidebarActivity');
+let moreLink = document.getElementById('showMoreLink');
+
+
+let toggleActivity = () => {
+    sidebarActivity.classList.toggle('open-activity');
+
+    if(sidebarActivity.classList.contains('open-activity')) {
+        moreLink.innerHTML = 'Show Less <b>-</b>';
+    }else {
+        moreLink.innerHTML = 'Show More <b>+</b>';
+    }
+}
